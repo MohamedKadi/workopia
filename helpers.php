c20dc4b8101316df9fab14b342cc4832589df041
MohamedKadi <gamesmohamed14@gmail.com>
2024-07-21 17:16:01 +0100

    setting home view document root +gitignore

---
+++ helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Get the base path
+ * 
+ * @param string $path
+ * 
+ * @return string
+ */
+function basePath($path = '')
+{
+    return __DIR__ . '/' . $path;   //ktrj3 full directory dyl file E:\MO COURSES SE\php\PHP From Scratch 2024 Beginner To Advanced\workopia 
+}
