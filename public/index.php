dc2b04185bed9dfc63ab640c5ffb2a3011555867
MohamedKadi <gamesmohamed14@gmail.com>
2024-07-22 18:17:00 +0100

    include database

--- public/index.php
+++ public/index.php
@@ -1,19 +1,19 @@
 <?php
 require '../helpers.php';
 require basePath('Database.php');
-
-$config = require basePath('config/db.php');
-
-$db = new Database($config);
-
 require basePath('Router.php');
 
+
+
+//Instatiating the router
 $router = new Router();
 
+//Get route
 $routes = require basePath('routes.php');
 
+//Get current uri & http method
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
-
+//route the request
 $router->route($uri, $method);
