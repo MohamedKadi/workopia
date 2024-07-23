03de897498fca9528d868a77dddd173dc7bcaf7a
MohamedKadi <gamesmohamed14@gmail.com>
2024-07-23 17:17:05 +0100

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mohamedkadi/workopia',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8d210514984a46a0a72148bc8c8871958b7b5749',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mohamedkadi/workopia' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8d210514984a46a0a72148bc8c8871958b7b5749',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
